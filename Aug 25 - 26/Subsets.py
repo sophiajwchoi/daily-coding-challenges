68a1e10549b26834c0ab0bf1963db9ff7bfa60cf
Sophia Jiwho Choi <choij12@mcmaster.ca>
2018-08-25 21:51:12 -0400

    Aug 25: finding the minimum sum path and power set

---
+++ Aug 25 - 26/Subsets.py
@@ -0,0 +1,18 @@
+##Given a set of distinct integers, nums, return all possible subsets (the power set).
+##
+##Note: The solution set must not contain duplicate subsets.
+##
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        subsets = []
+        for i in range(2 ** len(nums)):
+            subset = []
+            for k in range(len(nums)):
+                if i & 1 << k:
+                    subset.append(nums[k])
+            subsets.append(subset)
+        return subsets
