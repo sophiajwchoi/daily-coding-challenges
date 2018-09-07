f2f22df0153fd3a84f75f647f199e8bfe279413f
Sophia Jiwho Choi <choij12@mcmaster.ca>
2018-09-07 01:04:34 -0400

    Converting Hex to Binary completed

--- Aug 29 - 31/convert_hex_bin.py
+++ Aug 29 - 31/convert_hex_bin.py
@@ -1,19 +1,31 @@
-## work in progress
+## input type: str
+## output type: str
 
 def convert_hex_to_binary(hex_str):
-    return_bin = 0
-    for i in range(len(hex_str)):
-        if hex_str[i] in ['A','B','C','D','E','F']:
-            a = char_to_dec(x)
-        hex_str[i]
-        return_bin = a + return_bin
+    return_bin = ""
+    for i in hex_str:
+        if i in ['A','B','C','D','E','F']:
+            i = char_to_dec(i)
+        return_bin += dec_to_bin(i)
         
     return return_bin    
 
 def dec_to_bin(digit):
-    if digit == 0:
-        return 0000
-    while digit // 2 != 0:
+    digit2 = int(digit)
+    binary = ""
+    while digit2 // 2 > 0:
+        binary += str(digit2 % 2)
+        digit2 = digit2 // 2
+    binary = binary[::-1]     #Reverse the string
+    if digit2 // 2 == 0 and len(binary) != 4:
+        if digit2 == 1:
+            binary = "1" + binary
+        else:
+            binary = "0" + binary
+    offset = 4 - len(binary)
+    binary = "0" * offset + binary
+    
+    return binary
         
         
 def char_to_dec(hex_char):
