7888374a21b1ab19e34c728b0b45a800d6c5579f
Sophia Jiwho Choi <choij12@mcmaster.ca>
2018-09-10 21:11:00 -0400

    Finding the Diameter of a Binary Tree implemented

--- Sept 7 - 10/Diameter of Binary Tree.py
+++ Sept 7 - 10/Diameter of Binary Tree.py
@@ -11,17 +11,40 @@
 ##
 ##Note: The length of path between two nodes is represented by the number of edges between them.
 
- class Node:
+class Node:
      def __init__(self, x):
          self.val = x
          self.left = None
          self.right = None
 
-def diameterOfBinaryTree(root):
-    
+def diameter_BST(root):
+    if root != None:
+        l_height = getHeight(root.left)
+        r_height = getHeight(root.right)
 
+        l_diameter = diameter_BST(root.left)
+        r_diameter = diameter_BST(root.right)
+
+    else:
+        return 0
+    return max(l_height + r_height, max(l_diameter, r_diameter))
 
 
 def getHeight(node):
-    if node != None:
-        n
+    if node == None:
+        return 0
+    else:
+        return 1 + max(getHeight(node.left), getHeight(node.right))
+
+
+def main():
+    a = Node(1)
+    b = Node(2)
+    c = Node(3)
+    d = Node(4)
+    e = Node(5)
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    print(diameter_BST(a))
