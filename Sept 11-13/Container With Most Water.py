afc5e339efbc598f5749863dbac763aacd1fc9c5
Sophia Jiwho Choi <choij12@mcmaster.ca>
2018-09-17 23:13:25 -0400

    Container with Most water implemented using two pointers

--- Sept 11-13/Container With Most Water.py
+++ Sept 11-13/Container With Most Water.py
@@ -9,3 +9,24 @@
 
 
 def maxArea(li):
+    i = 0
+    j = len(li) - 1
+    if j < 0:
+        return 0
+    counter = 0
+    max_area = get_maxArea(i, j, li)
+    while i < j:
+        area = get_maxArea(i, j, li)
+        if area > max_area:
+            max_area = area
+        if counter == 0:
+            i += 1
+            counter += 1
+        else:
+            j -= 1
+            counter -= 1
+    return max_area
+
+def get_maxArea(i, j, li):
+    return (j - i) * min(li[i], li[j])
+        
